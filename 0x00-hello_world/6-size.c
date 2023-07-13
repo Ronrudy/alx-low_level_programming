3393b21dcdfd5b448f213d8f760032ae42857c3d
Ronrudy <Ruffroony@yahoo.com>
2023-07-13 05:05:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints the size of various types on the computer it is compiled and run on.
+ * main - A program that prints the size of various types on
+ * the computer it is compiled and run on.
  * Return: Always 0 (Success)
  */
 
