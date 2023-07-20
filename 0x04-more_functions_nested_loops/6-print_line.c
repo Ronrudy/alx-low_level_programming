6c74ae2a4c39d793e1f17aaf40d88f730f322ffb
Ronrudy <Ruffroony@yahoo.com>
2023-07-20 12:08:21 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line
+ * @n: number of times the character prints
+ * Return: 0
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	return;
+}
