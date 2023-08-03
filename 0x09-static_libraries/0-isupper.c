8253f5bd64f62477ede3d2e76eb20b18c22e5d70
Ronrudy <Ruffroony@yahoo.com>
2023-08-03 07:19:08 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: The character should be checked
+ * Return: 1 for uppercase character or
+ * 0 for anything otherwise
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: string to be checked
+ * Return: lenght of the string
+ */
+
+int _strlen(char *s)
+
+{
+	int string_lenght = 0;
+
+	while (s[string_lenght])
+		string_lenght++;
+	return (string_lenght);
+}
