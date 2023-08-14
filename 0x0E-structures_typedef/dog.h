7bbc4d93f3053251aa19551bd02217ef15a6498e
Ronrudy <Ruffroony@yahoo.com>
2023-08-14 14:35:52 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a strct that creates a dog info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
