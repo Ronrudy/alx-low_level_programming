5b2d1eaeecf50101559172822f94aeaf2a4e3f3b
Ronrudy <Ruffroony@yahoo.com>
2023-08-24 08:33:05 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,19 +6,20 @@
  *
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        h = h->next;
-        count++;
-    }
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
