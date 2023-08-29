8db36de615ba75cd195048c87b08b769a52774fe
Ronrudy <Ruffroony@yahoo.com>
2023-08-28 19:26:35 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -60,7 +60,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (tmp == NULL)
 		return (NULL);
 	if (idx == 0)
-		new_node->next = tmp->next;
+		new_node->next = tmp;
 	else
 	{
 		tmp_old = tmp->next;
