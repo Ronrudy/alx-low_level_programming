2667f91f7ec61e7431c3caaf7ef683d1ce2af011
Ronrudy <Ruffroony@yahoo.com>
2023-08-31 09:26:38 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,11 +14,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	mask = 1;
 	mask = mask << index;
-
 	if (index > sizeof(unsigned long int) * 8 || n == NULL)
 		return (-1);
 	if (((*n >> index) & 1) == 1)
 		*n = mask ^ *n;
 
-	return (-1);
+	return (1);
 }
